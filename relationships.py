19530f9f4f8c796209b8b46947f9198a774dc382
Ben Moran <bemoran@millersville.edu>
2022-02-02 17:40:28 -0500

    Added some preliminary code for relationships to get the import working between AClass.py and relationships.py

---
+++ relationships.py
@@ -0,0 +1,9 @@
+from AClass import AClass
+from AClass import listOfClasses
+
+
+def add(src: AClass, dest: AClass):
+    src.listOfRelationships.append(dest.name)
+
+def delete(src, dest):
+    return
