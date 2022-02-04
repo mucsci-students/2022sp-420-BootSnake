9b3da82c7ad3efd57d6c8b073f332ff91f1eac57
Ben Moran <bemoran@millersville.edu>
2022-02-04 18:01:25 -0500

    Changed add function to support classSearch for when that is pushed

--- relationships.py
+++ relationships.py
@@ -1,9 +1,17 @@
-from AClass import AClass
-from AClass import listOfClasses
+from asyncio.windows_events import NULL
+from AClass import *
 
 
-def add(src: AClass, dest: AClass):
-    src.listOfRelationships.append(dest.name)
+# def RelationshipAdd(src: AClass, dest: AClass):
+#     src.listOfRelationships.append(dest.name)
 
-def delete(src, dest):
+def RelationshipAdd(src: str, dest: str):
+    srcClass = ClassSearch(src)
+    destClass = ClassSearch(dest)
+    if srcClass is not None and destClass is not None:
+        srcClass.listOfRelationships.append(destClass.name)
+    
+"""
+"""
+def RelationshipDelete(src: str, dest: str):
     return
