5a6c1363ebd698a7ee763d6850d49cc1e1853f2d
Amelia G Spanier <agspanie@csciarch03.muad.local>
2022-02-06 16:35:35 -0500

    Wrote Help() and added help.txt Help is written so that it reads from text file help.txt which will contain instructions for using the program. It is written this way so we won't have a large block of text in our code.

--- interface.py
+++ interface.py
@@ -16,7 +16,14 @@ def ListClasses():
 def ListClass():
     return
 
+"""
+This function reads a text file of helpful instructions for the user and
+prints it
+"""
 def Help():
+    helpfile = open('help.txt', 'r')                        # Open text file as read-only
+    print(helpfile.read())                                  # Read file & print to terminal
+    helpfile.close()
     return
 
 def Exit():
