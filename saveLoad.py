8ab9c79e5b1391fa93ec1dfaedf9d99d38d35950
Travis W Zehring <twzehrin@csciarch02.muad.local>
2022-02-07 20:28:45 -0500

    Added the saveLoad file and set up the save and load functions

---
+++ saveLoad.py
@@ -0,0 +1,20 @@
+"""
+This file is a class used to implement a save and load function by reading and writing
+to JSON files
+"""
+
+# Imports
+from AClass import *
+import json 
+
+"""
+Save to a JSON format file, make sure to save a valid name
+"""
+def Save ():
+    return
+
+"""
+Load a JSON format file, only if one has already been saved
+"""
+def Load ():
+    return
