b242d24613001085bbf3ac7249fa5753542a3604
Ben Moran <benmoran@Bens-MacBook-Air.local>
2022-03-01 18:33:02 -0500

    Added calls to functions.

--- interfaceGUI.py
+++ interfaceGUI.py
@@ -4,14 +4,22 @@
 # last edited: 2/23/22
 
 
+from crypt import methods
 from tkinter import *
 from tkinter import messagebox
+from tkinter import filedialog
 import tkinter as tk
 from tkinter import ttk
 
+from AClass import *
+from relationships import *
+from saveLoad import *
+from UML_attributes import *
+
+
 window = tk.Tk()
 window.title("bootsnake")
-window.geometry("250x200")
+window.geometry("400x200")
 
 
 
@@ -47,11 +55,48 @@ methodsOptions = [
 
 
 
-def test(*args):
+def classCalls(*args):
     if classVar.get() == 'Add':
-        messagebox.showinfo(
-            "info name", "class add"
-        )
+        ClassAdd()
+    elif classVar.get() == 'Delete':
+        ClassDelete()
+    elif classVar.get() == 'Rename':
+        ClassRename()
+
+def relationshipCalls(*args):
+    if relationshipVar.get() == 'Add':
+        RelationshipAdd()
+    elif relationshipVar.get() == 'Delete':
+        RelationshipDelete()
+
+
+def methodCalls(*args):
+    if methodsVar.get() == 'Add':
+        return
+    elif methodsVar.get() == 'Delete':
+        return
+    elif methodsVar.get() == 'Rename':
+        return
+
+
+def fieldCalls(*args):
+    if fieldsVar.get() == 'Add':
+        return
+    elif fieldsVar.get() == 'Delete':
+        return
+    elif fieldsVar.get() == 'Rename':
+        return
+
+
+def saveCall():
+    filename = filedialog.asksaveasfilename()
+    save(filename)
+
+def loadCall():
+    filename = filedialog.askopenfilename(filetypes=(("Json files", "*.json*")))
+    #filename = filedialog.askopenfilename(filetypes=(("Json files", "*.json*")))
+    load(filename)
+
 
 
 classLabel = tk.Label(window, text="Class").grid(row=0, column=0)
@@ -68,10 +113,18 @@ methodsLabel = tk.Label(window, text="Methods").grid(row=0, column=3)
 methodsDrop = tk.OptionMenu(window, methodsVar, *methodsOptions).grid(row=1, column=3)
 # startButton = tk.Button(window, text="Start", command=test).grid(row=1, column=2)
 
+saveButton = tk.Button(window, text="Save", command=saveCall).grid(row=1, column=4)
+loadButton = tk.Button(window, text="Load", command=loadCall).grid(row=1, column=5)
 
 
 
-classVar.trace("w", test)
+
+classVar.trace("w", classCalls)
+relationshipVar.trace("w", relationshipCalls)
+methodsVar.trace("w", relationshipCalls)
+fieldsVar.trace("w", methodCalls)
+
+
 
 # end
 window.mainloop()
