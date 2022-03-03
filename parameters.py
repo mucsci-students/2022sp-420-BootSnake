63cfda4faf37009448bc1551860a976a62b10239
Amelia G Spanier <agspanie@csciarch03.muad.local>
2022-03-03 12:11:59 -0500

    Wrote ParamDelete method ParamDelete() first checks if the user wishes to delete all parameters. If so, the list is cleared. Otherwise, the list will be searched for the requested param to be deleted. Closes #41

--- parameters.py
+++ parameters.py
@@ -16,44 +16,6 @@ class Param:
         self.type = paramType
 
 """
-CheckNameType
-Input: given parameter name, given parameter type, method expecting param addition
-Description: Checks that both name & type of the new param are valid and that a param with the same
-name does not exist within the method. Both name & type must be valid in order for the param to be
-added to the method's list of params
-"""
-def CheckNameType(paramName: str, paramType: str, methodName):
-        
-    if (not paramName.strip()) or (not paramType.strip()):  
-        print("UML> Name cannot be blank!")
-        return False
-            
-            
-    elif (regex.search(paramName.strip()) != None) or (regex.search(paramType.strip()) != None):
-        print("UML> No special characters allowed!")
-        return False
-            
-    elif (paramName[:1].strip().isnumeric()) or (paramType[:1].strip().isnumeric()): 
-        print("UML> Param name and type cannot be preceded by an integer(s)!")
-        return False
-    
-            
-    elif (keyword.iskeyword(paramName.strip())) or (keyword.iskeyword(paramType.strip())):      
-        print("UML> Keywords are not allowed!")
-        return False
-
-    elif (match.search(paramName.strip()) != None) or (match.search(paramType.strip()) != None):
-        print("UML> No space allowed! Use an underscore!")
-        return False
-            
-    else:
-        for o in methodName.listOfParams:
-            if o.name.lower().strip() == paramName.lower().strip():
-                print("UML> No duplicates allowed! Method(s) must be unique!")
-                return False
-        return True
-
-"""
 ParamAdd
 Input: name of class containing method, name of method to add to, parameter's name, parameter's type
 Description: Creates a parameter with a valid name & type and appends to a given method's parameter list
@@ -104,3 +66,65 @@ def ParamListAdd(wantedMethod, paramName, paramType):
 
     else:
         return None
+
+"""
+ParamDelete
+Input: method expecting param deletion, whether user wants to delete one or all parameters in the list,
+parameter's name (empty if ALL delete)
+Description: Deletes one or all params from a given method
+"""
+def ParamDelete(wantedMethod, delAmnt, paramName):
+    
+    if wantedMethod.listOfParams: 
+        if delAmnt == 'all':
+            wantedMethod.listOfParams.clear()           # If user wants to delete all params, clear list
+            print("All parameters successfully deleted!")
+            print(wantedMethod.listOfParams)
+
+        elif delAmnt == 'one':
+            for param in wantedMethod.listOfParams:
+                if param.name.casefold().strip() == paramName.casefold().strip():
+                    wantedMethod.listOfParams.remove(param.name)
+                    print("UML> Attribute deleted!")
+                    print(wantedMethod.listOfParams)
+                    break
+    else:
+        print("No params exist in this method!")
+
+"""
+CheckNameType
+Input: given parameter name, given parameter type, method expecting param addition
+Description: Checks that both name & type of the new param are valid and that a param with the same
+name does not exist within the method. Both name & type must be valid in order for the param to be
+added to the method's list of params
+"""
+def CheckNameType(paramName: str, paramType: str, methodName):
+        
+    if (not paramName.strip()) or (not paramType.strip()):  
+        print("UML> Name cannot be blank!")
+        return False
+            
+            
+    elif (regex.search(paramName.strip()) != None) or (regex.search(paramType.strip()) != None):
+        print("UML> No special characters allowed!")
+        return False
+            
+    elif (paramName[:1].strip().isnumeric()) or (paramType[:1].strip().isnumeric()): 
+        print("UML> Param name and type cannot be preceded by an integer(s)!")
+        return False
+    
+            
+    elif (keyword.iskeyword(paramName.strip())) or (keyword.iskeyword(paramType.strip())):      
+        print("UML> Keywords are not allowed!")
+        return False
+
+    elif (match.search(paramName.strip()) != None) or (match.search(paramType.strip()) != None):
+        print("UML> No space allowed! Use an underscore!")
+        return False
+            
+    else:
+        for o in methodName.listOfParams:
+            if o.name.lower().strip() == paramName.lower().strip():
+                print("UML> No duplicates allowed! Method(s) must be unique!")
+                return False
+        return True
