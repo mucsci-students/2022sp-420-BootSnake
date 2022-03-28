a15d8b1050fc38afe3759d9a462f7a2b18d63556
Andy Pham <anpham@millersville.edu>
2022-03-28 14:35:12 -0400

    fixed test_classModel maybe

--- tests/test_classModel.py
+++ tests/test_classModel.py
@@ -23,32 +23,27 @@ import sys
 import io
 
 def test_classAdd_ValidName():
+    listOfClasses.clear()
     listOfInputs = ['hi','HI','Hi','Hi1','h2o','Hello_World','Trailing__']
     index = 0
-    numOfFailure = 0
     while(index < len(listOfInputs)):
         #The following two lines will suppress the text from ClassAdd()
         suppress_text = io.StringIO()
         sys.stdout = suppress_text 
-        
         ClassAdd(listOfInputs[index])
         sys.stdout = sys.__stdout__
         result = len(listOfClasses)
-        try:
-            assert result == 1
-            listOfClasses.remove(ClassSearch(listOfInputs[index], listOfClasses))
-        except:
-            numOfFailure = numOfFailure + 1
         index = index + 1
-    return numOfFailure
+    assert result == 7
+
 
 def test_classAdd_InvalidName():
     duplicate = AClass('duplicate')
+    listOfClasses.clear()
     listOfClasses.append(duplicate)
     listOfInputs = ['duplicate','1NumberFirst','','    Spaces','Hello World','Hello~','Hello`','Hello_World!','@twitter','#Hashtag','HollarHollarGet$','Hundred%','Super^Script','D&D','*_*','I8(','I8)','__Leading','Fire-Emblem','New_Game+','=3','W[','WOh]','NO{','SDF}','SEMI;','COLON:','SINGLEQUOTE\'','DOUBLEQUOTE\"','VER|TICAL','BACKSLASH\\','C,OMMA','LESSER<','PER.IOD','>GREENTEXT','/FORWARDSLASH','QUE?','if','class','else','while','elif','def','for']
     
     index = 0
-    numOfFailure = 0
     while(index < len(listOfInputs)):
         #The following two lines will suppress the text from classAdd()
         suppress_text = io.StringIO()
@@ -57,26 +52,17 @@ def test_classAdd_InvalidName():
         sys.stdout = sys.__stdout__
         result = len(listOfClasses)
         #We will try the assert, if it fails we go to except.
-        try:
-            assert result == 1
-        except:
-            listOfClasses.remove(ClassSearch(listOfInputs[index], listOfClasses))
-            numOfFailure = numOfFailure + 1
+        assert result == 1
         index = index + 1
-    listOfClasses.remove(duplicate)
-    return numOfFailure
 
 def test_classRename_validRename():
-
-    
+    listOfClasses.clear()
     class1 = AClass("class1")
     class2 = AClass("class2")
     listOfClasses.append(class1)
     listOfClasses.append(class2)
     relation = relationship("Aggregation", class2.name)
     listOfClasses[0].listOfRelationships.append(relation)
-
-    
     #The following two lines will suppress the text from classAdd()
     suppress_text = io.StringIO()
     sys.stdout = suppress_text
@@ -89,6 +75,7 @@ def test_classRename_validRename():
     listOfClasses.clear()
 
 def test_classDelete_validDelete():
+    listOfClasses.clear()
     class1 = AClass("dontDeleteMePlz")
     listOfClasses.append(class1)
     #The following two lines will suppress the text from classAdd()
