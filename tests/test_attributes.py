4cf69a82e75af4bfe5cf45124748cb0774f6ec2c
tttrinh <tttrinh@millersville.edu>
2022-04-08 10:33:51 -0400

    change test_attributes.py

--- tests/test_attributes.py
+++ tests/test_attributes.py
@@ -12,6 +12,8 @@ import pytest
 from classModel import *
 from attributesModel import *
 from parametersModel import *
+from sharedItems import *
+from relationshipsModel import *
 
 
 '''
@@ -25,177 +27,284 @@ from parametersModel import *
     Do the following steps to run the pytest in the installed virtual env.
     
         1. Activate the virtual environment by typing the command: 
-            source 'virtual_env_name'/bin/activate 
+                    source 'virtual_env_name'/bin/activate 
         2. Run pytest by entering the command:    'python -m pytest' 
 '''
-
+def reset():
+    listOfClasses.clear()
 # This file contains tests that also validate the integrations of 
 # various modules by calling functions from other files.
 
+
+def test_addAClass():
+    ClassAdd("classFruit")
+    assert "classFruit successfully added!"
+    assert "classFruit" in listOfClasses[0]
+    assert listOfClasses[0].name =="classFruit"
+    
+
 # test add a field to a specific class
 def test_addField():
-    thisClass = AClass('classFruit')
-    thisClass.listOfFields = list()
-    thisField = FieldClass('apple', 'fruit')
-    if checkName(thisClass,'apple') and checkName(thisClass, 'fruit'):
-        thisClass.listOfFields.append(thisField)
-        assert 'Field successfully added!'
-       
+    ClassAdd("classFruit")
+    addField("classFruit","apple", "str")
+    assert "classFruit" in listOfClasses[0]
+    assert listOfClasses[0].name =="classFruit"
+    assert "Field successfully added!"
+    assert listOfClasses[0].listOfField[0].name =="apple"
+      
 # test add a duplicate field to a specific class
 def test_addDupField():
-    thisClass = AClass('classFruit')
-    thisClass.listOfFields = list()
-    thisField = FieldClass('apple', 'fruit')
-    if checkName(thisClass,'apple') and checkName(thisClass, 'fruit'):
-        thisClass.listOfFields.append(thisField)
-        assert 'No Duplicate Field Allowed!'
-        
+    reset()
+    ClassAdd("classFruit")
+    addField("classFruit","apple", "str")
+    addField("classFruit","apple", "str")
+    assert "classFruit" in listOfClasses[0]
+    assert listOfClasses[0].name =="classFruit"
+    assert "Field successfully added!"
+    assert listOfClasses[0].listOfField[0].name =="apple"
+    assert "apple existed! No duplicates allowed!"
+    
 
 # test add a blank field & empty type to a specific class
 def test_addBlankField():
-    thisClass = AClass('classFruit')
-    thisClass.listOfFields = list()
-    thisField = FieldClass('', '')
-    if checkName(thisClass,'') and checkName(thisClass, ''):
-        thisClass.listOfFields.append(thisField)
-        assert 'Field and Type can''t'' be blank!'
-        
+    reset()
+    ClassAdd("classFruit")
+    addField("classFruit"," ", "str")
+    assert "classFruit" in listOfClasses[0]
+    assert listOfClasses[0].name =="classFruit"
+    assert "name and type cannot be blank!"
+    assert listOfClasses[0].listOfField[0]== None
  
+
+# test add a field to an invalid class
+def test_addFieldInvalidClass():
+    ClassAdd("classFruit")
+    addField("classFoo","apple", "str")
+    assert "classFruit" in listOfClasses[0]
+    assert listOfClasses[0].name =="classFruit"
+    assert "classFoo not existed! Enter a valid class!"
+    assert listOfClasses[0].name == "classFruit"
+    assert listOfClasses[0].listOfField[0]== None
+
+
 # test rename a field to a specific class
 def test_renameField():
-    thisClass = AClass('classFruit')
-    thisClass.listOfFields = list()
-    thisField = FieldClass('apple', 'fruit')
-    if checkName(thisClass,'kiwi') and checkName(thisClass, 'kiwi'):
-        renField('classFruit', 'apple','kiwi')
-        assert 'Field successfully renamed!'       
-
+    reset()
+    ClassAdd("classFruit")
+    addField("classFruit","apple", "str")
+    
+    assert "classFruit" in listOfClasses[0]
+    assert listOfClasses[0].name =="classFruit"
+    assert "Field successfully added!"
+    assert listOfClasses[0].listOfField[0].name =="apple"
+    renField("classFruit", "apple","kiwi")
+    assert  "apple successfully renamed to kiwi!"
+    assert listOfClasses[0].listOfField[0].name =="kiwi"
+          
 
 # test rename an invalid field to a specific class
 def test_renameInvalidField():
-    thisClass = AClass('classFruit')
-    thisClass.listOfFields = list()
-    thisField = FieldClass('apple', 'fruit')
-    if checkName(thisClass,'kiwi') and checkName(thisClass, 'kiwi'):
-        renField('classFruit', 'banana','kiwi')
-        assert 'Field ' +'banana' + 'not found for class ' + 'classFruit' + '!'         
-
-
+    reset()
+    ClassAdd("classFruit")
+    addField("classFruit","apple", "str")
+    assert "Field successfully added!"
+    assert "classFruit" in listOfClasses[0]
+    assert listOfClasses[0].name =="classFruit"
+    assert listOfClasses[0].listOfField[0].name =="apple"
+    renField("classFruit", "kiwi","pear")  
+    assert  "Field kiwi not found!"
+    assert listOfClasses[0].listOfField[0].name =="apple"
+        
+# test rename a field to an invalid class
+def test_renameFieldInvalidClass():
+    reset()
+    ClassAdd("classFruit")
+    addField("classFruit","apple", "str")
+    assert "Field successfully added!"
+    assert "classFruit" in listOfClasses[0]
+    assert listOfClasses[0].name =="classFruit"
+    assert listOfClasses[0].listOfField[0].name =="apple"
+    renField("classFoo", "kiwi","pear") 
+    assert "classFoo not existed! Enter a valid class!"
+    assert listOfClasses[0].name == "classFruit"
+    assert listOfClasses[0].listOfField[0].name =="apple"
+        
 # test delete a field to a specific class
 def test_delField():
-    thisClass = AClass('classFruit')
-    thisClass.listOfFields = list()
-    thisField = FieldClass('apple', 'fruit')
-    delObj = searchField('classFruit', 'apple')
-    if delObj:
-        thisClass.listOfFields.remove(delObj)
-        assert 'Field ' +'apple' + 'successfully deleted!'
+    reset()
+    ClassAdd("classFruit")
+    addField("classFruit","apple", "str")
+    assert "Field successfully added!"
+    assert "classFruit" in listOfClasses[0]
+    assert listOfClasses[0].name =="classFruit"
+    assert listOfClasses[0].listOfField[0].name =="apple"
+    delField("classFruit", "apple")
+    assert  "Field apple successfully deleted!"
+    assert listOfClasses[0].listOfField[0]== None
         
         
 # test delete an invalid field to a specific class
 def test_delInvalidField():
-    thisClass = AClass('classFruit')
-    thisClass.listOfFields = list()
-    thisField = FieldClass('apple', 'fruit')
-    delObj = searchField('classFruit', 'kiwi')
-    if delObj:
-        thisClass.listOfFields.remove(delObj)
-        assert 'Field ' +'kiwi' + 'not found!'
+    reset()
+    ClassAdd("classFruit")
+    addField("classFruit","apple", "str")
+    assert "Field successfully added!"
+    assert "classFruit" in listOfClasses[0]
+    assert listOfClasses[0].name =="classFruit"
+    assert listOfClasses[0].listOfField[0].name =="apple"
+    delField("classFruit", "kiwi")
+    assert  "Field kiwi not found! Try again!"
+    assert listOfClasses[0].listOfField[0].name =="apple"
 
+# test delete all fields to a specific class
+def test_delAllFields():
+    reset()
+    ClassAdd("classFruit")
+    addField("classFruit","apple", "str")
+    assert "Field successfully added!"
+    assert "classFruit" in listOfClasses[0]
+    assert listOfClasses[0].name =="classFruit"
+    assert listOfClasses[0].listOfField[0].name =="apple"
+    addField("classFruit","kiwi", "str")
+    assert "Field successfully added!"
+    assert listOfClasses[0].listOfField[1].name =="kiwi"
+    delField("classFruit", "all")
+    assert  "All Fields successfully deleted!"
+    assert listOfClasses[0].listOfField[0] == None
 
+# test delete a field to an invalid class
+def test_delFieldInvalidClass():
+    reset()
+    ClassAdd("classFruit")
+    addField("classFruit","apple", "str")
+    assert "Field successfully added!"
+    assert "classFruit" in listOfClasses[0]
+    assert listOfClasses[0].name =="classFruit"
+    assert listOfClasses[0].listOfField[0].name =="apple"
+    delField("classFood", "all")
+    assert "classFoo not existed! Enter a valid class!"
+    assert listOfClasses[0].name == "classFruit"
+    assert listOfClasses[0].listOfField[0].name =="apple"
 
 
 # test add a method to a specific class
 def test_addMethod():
-    thisClass = ClassAdd('classFruit')
-    thisMethod = MethodClass('Apple', 'fruit')
-    if checkMethName(thisClass,'') and checkMethName(thisClass, ''):
-        thisClass.listOfMethods.append(thisMethod)
-        assert 'Method successfully added!'
-        
+    reset()
+    ClassAdd("classFood")
+    addMethod("classFood","beef", "str", [])
+    assert "classFood successfully added!"
+    assert "classFood" in listOfClasses[0]
+    assert listOfClasses[0].name =="classFood"
+    assert "Beef successfully added!"
+    assert listOfClasses[0].listOfMethods[0].name =="Beef"
 
 # test add a duplicate method to a specific class
 def test_addDupMethod():
-    thisClass = ClassAdd('classFruit')
-    thisMethod = MethodClass('Apple', 'fruit')
-    if checkMethName(thisClass,'') and checkMethName(thisClass, ''):
-        thisClass.listOfMethods.append(thisMethod)
-        assert 'No Duplicate Method Allowed!'
+    reset()
+    ClassAdd("classFood")
+    addMethod("classFood","beef", "str", [])
+    assert "Beef successfully added!"
+    assert listOfClasses[0].listOfMethods[0].name =="Beef"
+    addMethod("classFood","beef", "str", [])
+    assert "beef existed! No duplicated allowed!" 
+    assert listOfClasses[0].listOfMethods[0].name =="Beef"
         
 
-# test add a method to a specific class
+# test add a blank method to a specific class
 def test_addBlankMethod():
-    thisClass = ClassAdd('classFruit')
-    thisMethod = MethodClass('', '')
-    if checkMethName(thisClass,'') and checkMethName(thisClass, ''):
-        thisClass.listOfMethods.append(thisMethod)
-        assert 'Method name and type can''t'' be blank!'
-        
-
+    reset()
+    ClassAdd("classFood")
+    addMethod("classFood"," ", "str",[])
+    assert "classFruit successfully added!"
+    assert "classFruit" in listOfClasses[0]
+    assert listOfClasses[0].name =="classFruit"
+    assert "Name and/or type cannot be blank!"
+    assert listOfClasses[0].listOfMethods[0] == None
+    
 
 # test rename a method to a specific class
 def test_reNameMethod():
-    thisClass = AClass('classFruit')
-    thisMethod = MethodClass('Apple', 'fruit')
-    
-    if checkMethName(thisClass, 'Kiwi'):
-        renMethod ('classFruit', 'Apple', 'Kiwi')
-        assert 'Method ' + 'Apple' + ' successfully renamed!'
+    reset()
+    ClassAdd("classFood")
+    addMethod("classFood","Beef", "str",[])
+    assert "Beef successfully added!"
+    assert listOfClasses[0].listOfMethods[0].name =="Beef"
+    renMethod("classFood","beef","soup" )
+    assert "Method beef successfully renamed to Soup!"
+    assert listOfClasses[0].listOfMethods[0] == "Soup"
 
 
-
-# test rename a method to a specific class
+# test rename an invalid method to a specific class
 def test_renameInvalidMethod():
-    thisClass = AClass('classFruit')
-    thisMethod = MethodClass('Apple', 'fruit')
-    
-    if checkMethName(thisClass, 'Kiwi'):
-        renMethod ('classFruit', 'Banana', 'Kiwi')
-        assert 'Method ' +'Banana' + ' Not found for class ' + 'classFruit' + '!'         
+    reset()
+    ClassAdd("classFood")
+    addMethod("classFood","Beef", "str",[])
+    assert "Beef successfully added!"
+    assert listOfClasses[0].listOfMethods[0].name =="Beef"
+    renMethod("classFood","soup","chicken")
+    assert "Method soup not found!"
+    assert listOfClasses[0].listOfMethods[0] == "Beef"
+        
         
 # test delete a method to a specific class
 def test_delMethod():
-    thisClass = AClass('classFruit')
-    thisMethod = MethodClass('Apple', 'fruit')
+    reset()
+    ClassAdd("classFood")
+    addMethod("classFood","Beef", "str",[])
+    assert "Beef successfully added!"
+    assert listOfClasses[0].listOfMethods[0].name =="Beef"
+    delMethod("classFood","beef")
+    assert "beef of classFood deleted!"
+    assert listOfClasses[0].listOfMethods[0] == None
     
-    if searchMethod( 'classFruit', 'Apple'):
-        for o in thisClass.listOfMethods:
-            if o.name == 'Apple'.title().strip():
-                o.listOfMethod.remove(o)
-                assert 'Method ' + 'Apple' + ' successfully deleted!'
-    else:
-        thisClass.listOfMethods.clear()
-        assert 'All methods of ' + 'classFruit' + 'successfully deleted!'
+# test delete all methods to a specific class
+def test_delAllMethods():
+    reset()
+    ClassAdd("classFood")
+    addMethod("classFood","Beef", "str",[])
+    assert "Beef successfully added!"
+    assert listOfClasses[0].listOfMethods[0].name =="Beef"
+    addMethod("classFood","soup", "str",[])
+    assert "soup successfully added!"
+    assert listOfClasses[0].listOfMethods[1].name =="Soup"
+    delMethod("classFood","all")
+    assert "All methods of classFood deleted!"
+    assert listOfClasses[0].listOfMethods[0] == None
+    
         
 # test change the Parameter of a given method in a class.
 # this test verifies the integration of other module besides
 # AClass
-def test_changeParam():
-    thisClass = AClass('classFruit')
-    thisMethod = MethodClass('Apple', 'fruit')
-    delParam ('classFruit', 'Apple', 'fuji') 
-    if delParam:
-        assert 'Parameter ' + 'fuji' + 'successfully deleted!'
-        paramChange = 'pink lady'
-        if paramChange.strip().casefold() != 'q':
-            changeType = 'crisp'
-            if changeType.strip().casefold() != 'q':
-                ParamAdd('classFruit', 'Apple'.title(), paramChange, changeType) 
-                assert 'Parameter ' + paramChange + ' successfully added!'
-                
-
+def test_delParam():
+    reset()
+    ClassAdd("classFood")
+    addMethod("classFood","Beef", "str",[])
+    ParamAdd("classFood","Beef","param", "str")
+    assert listOfClasses[0].listOfMethods[0].listOfParam[0].name == "param"
+    delParam("classFood","Beef","param")
+    assert "param of classFood deleted!"
+    assert listOfClasses[0].listOfMethods[0].listOfParam[0] == None
+    
 # test rename a parameter of a given method in provided class.
 def test_renameParam():
-    thisClass = ClassAdd('classFruit')
-    thisMethod = searchMethod('classFruit', 'Banana')
-    if thisMethod:
-        thisParam = searchParam(thisMethod, 'yellow'.strip().lower())
-        if thisParam:
-            if thisParam.name.strip().lower() == 'yellow'.strip().lower():    
-                if checkParamName(thisMethod, 'green'.strip().casefold()):
-                    thisParam.name = 'green'.lower().strip()
-                    assert 'Parameter '+ 'Banana' +' successfully renamed!'
+    reset()
+    ClassAdd("classFood")
+    addMethod("classFood","Beef", "str",[])
+    ParamAdd("classFood","Beef","param", "str")
+    assert listOfClasses[0].listOfMethods[0].listOfParam[0].name == "param"
+    renameParam("classFood","Beef","param", "myparam")
+    assert "param successfully renamed to myparam!"
+    assert listOfClasses[0].listOfMethods[0].listOfParam[0] == "myparam"
     
-             
-    
+# test rename an invalid parameter of a given method in provided class.
+def test_renameInvalidParam():
+    reset()
+    ClassAdd("classFood")
+    addMethod("classFood","Beef", "str",[])
+    ParamAdd("classFood","Beef","param", "str")
+    assert listOfClasses[0].listOfMethods[0].listOfParam[0].name == "param"
+    renameParam("classFood","Beef","param1", "myparam")
+    assert "param1 not found! Try again!"
+    assert listOfClasses[0].listOfMethods[0].listOfParam[0] == "param"
    
+    
